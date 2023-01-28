908f68071db8dbcfa39f9c36582145c1c3edc793
Abdussomad20 <harzkid2018@gmail.com>
2023-01-28 21:59:58 +0100

    promises jav

---
+++ promise.js
@@ -0,0 +1,30 @@
+//Callback
+const doSomething = callback => {
+  setTimeout(() => {
+    const skills = ['HTML', 'CSS', 'JS']
+    callback('It did not go well', skills)
+  }, 2000)
+}
+
+const callback = (err, result) => {
+  if (err) {
+    return console.log(err)
+  }
+  return console.log(result)
+}
+
+doSomething(callback)
+
+const doSomething = callback => {
+  setTimeout(() => {
+    const skills = ['HTML', 'CSS', 'JS']
+    callback(false, skills)
+  }, 2000)
+}
+
+doSomething((err, result) => {
+  if (err) {
+    return console.log(err)
+  }
+  return console.log(result)
+})
